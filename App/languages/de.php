3bc32d51ac5089221d64f955b95e09efa0bbd75b
a7 <a7@a7-MS-7A32>
2024-09-29 15:56:06 +0200

    Update Database.db, de.php, en.php, and 2 more files

--- App/languages/de.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'wrong_captcha' => 'Die Captcha-Eingabe ist falsch.',
-    'content_successfully_inserted' => 'Inhalt "%s" mit der ID "%s" wurde erfolgreich eingefügt.',
-    'label_image_preview' => 'Bildvorschau'
-];
--- App/languages/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'wrong_captcha' => 'Captcha input is incorrect.',
-    'content_successfully_inserted' => 'Content "%s" with id "%s" was successfully inserted.',
-    'label_image_preview' => 'Image preview'
-];
